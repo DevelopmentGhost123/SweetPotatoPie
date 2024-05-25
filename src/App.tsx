433588b47f11df556da6a8db378740d2fe0dbcd5
DevelopmentGhost123 <oniks0261@gmail.com>
2024-05-24 19:55:49 -0700

    Completed

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import About from "./componens/About/About";
 import { Header } from "./componens/Header/Header";
 import { Home } from "./componens/Home/Home";
 import Partnerships from "./componens/Partnerships/Partnerships";
-import "./index.css";
+import "./App.css";
+import "./fonts.css";
 
 function App() {
   return (
